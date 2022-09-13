7aa3202210f0b588679496206d5fcda4046dde9a
elijah <elijahkungu100@gmail.com>
2022-09-13 10:15:25 +0300

    day7 30days hanckertank

---
+++ HackerRank/print_odd_even_from_string.py
@@ -0,0 +1,15 @@
+s = "Helllo world"
+
+odd_string = []
+even_string = []
+len_string = len(s)
+# for i in range(len(s)):
+#     if(i% 2 == 0):
+#         even_string.append(i)
+#     else:
+#         odd_string.append(i)
+
+
+
+
+    
