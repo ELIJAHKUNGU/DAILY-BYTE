a19275227fb6bddd9d137d5f6d93382a73f36ee4
elijah <elijahkungu100@gmail.com>
2023-01-05 22:25:17 +0300

    recursive function

--- recap/savingEachCashDay365.py
+++ recap/savingEachCashDay365.py
@@ -6,7 +6,18 @@ for i in range(0,366):
 SumOf = sum(items)
 print(SumOf)
     
+# Add all numbers from 0 to 365 and print the result.
 
+def sumOfNumbers(n):
+    # explain = "Add all numbers from 0 to " + str(n) + " and print the result."
+    if n == 0:
+        return 0
+    # explain the code here why n-1 is used
+    # it is because the function is recursive and it will keep calling itself until it reaches the base case
+    # the base case is when n is equal to 0
 
+    return n + sumOfNumbers(n-1)
+    
+print(sumOfNumbers(365))
 
 
