7bfbdf0a143a7ab5531c06586b55485a7d66e39f
elijah <elijahkungu100@gmail.com>
2023-01-12 11:11:05 +0300

    array digits even, 1d array

---
+++ Google/ArrayProblem/arraydigit.py
@@ -0,0 +1,30 @@
+# Given an integer array, nums, return the total number of integers within nums 
+# that have an even number of digits.
+
+# Ex: Given the following nums…
+
+# nums = [1, 12, 123], return 1 (12 is the only integer with an even number of digits).
+# Ex: Given the following nums…
+
+# nums = [1, 32, 3492, 23], return 3.
+
+def integer_digits_arr(nums):
+    count = 0
+    for num in nums:
+        # print(num)
+        converting_digit_String = str(num)
+        if len(converting_digit_String) % 2 == 0 :
+            count += 1
+    return print(count)
+
+# Using the list Compression
+
+def integer_digits_arr_compression(nums):
+    return  print(len([num for num in nums if len(str(num)) % 2 ==0  ]) )
+
+if __name__ == "__main__":
+    integer_digits_arr([1, 12, 123])
+    integer_digits_arr([1, 32, 3492, 23])
+    integer_digits_arr_compression([1, 32, 3492, 23])
+    integer_digits_arr_compression([1, 12, 123])
+
---
+++ Leetcode/ArrayProblem/1dsumArray.py
@@ -0,0 +1,16 @@
+# Given an array nums. We define a running sum of an array as
+# runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums)
+        ans[0] = nums[0]
+        for i in range(1, len(nums)):
+            ans[i] = ans[i-1] + nums[i]
+        return ans
