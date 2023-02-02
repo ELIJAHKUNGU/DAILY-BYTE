690314913f7f895067f4dcd0dc800954f4b2a164
elijah <elijahkungu100@gmail.com>
2023-02-02 11:05:37 +0300

    headelementslinked list

---
+++ recap/headandelementslinked.py
@@ -0,0 +1,9 @@
+# Given a reference to the head of a linked list and two values, m, and n, traverse the entire list 
+# keeping the first m elements followed by removing the next n elements. Return the resulting list.
+
+# Ex: Given the following head, m, and n…
+
+# 1 -> 2 -> 3 -> null, m = 1, n = 1, return 1 -> 3 -> null.
+# Ex: Given the following head, m, and n…
+
+# 1 -> 3 -> 2 -> 4 -> null, m = 2, n = 2, return 1 -> 3 -> null.
