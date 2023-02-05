7266c202d39d695f85a862054fc73f550c047a8f
elijah <elijahkungu100@gmail.com>
2023-02-05 21:48:47 +0300

    array length

---
+++ Google/ArrayProblem/arraylength.py
@@ -0,0 +1,18 @@
+# Given an integer array, nums, and an integer, k,
+#  return whether or not it’s possible to divide nums
+#  into groups of size k where every group contains consecutive numbers.
+
+# Ex: Given the following nums and k…
+
+# nums = [1, 2, 2, 1], k = 2, return true ([1, 2] and [1, 2]).
+# Ex: Given the following nums and k…
+
+# nums = [1, 2, 3, 3], k = 2, return false.
+
+def array_length(nums, k):
+    pass
+    
+
+
+if __name__ == "__main__":
+    array_length(nums = [1, 2, 3, 3], k = 2)
