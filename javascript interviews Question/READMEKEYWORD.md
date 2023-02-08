1ea82d2ba08423f4809f02b0dcbe322a80c2ced3
elijah <elijahkungu100@gmail.com>
2023-02-08 10:24:17 +0300

    keyword

---
+++ javascript interviews Question/READMEKEYWORD.md
@@ -0,0 +1,37 @@
+## LET 
+
+ The `let` statement declares a block scope local variable.
+ Hence the variables defined with let keyword are limited
+ in scope to the block, statement, or expression on which
+ it is used.
+ 
+ ## VAR
+  Whereas variables declared with the `var` 
+ keyword used to define a variable globally, or locally
+  to an entire function  regardless of block scope.
+
+  ## CONST 
+  
+
+ <table>
+      <tr>
+        <th>Var</th>
+        <th>let</th>
+        <th>const</th>
+      </tr>
+      <tr>
+        <td>It is been available from the beginning of JavaScript</td>
+        <td>Introduced as part of ES6</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>It has function scope</td>
+        <td>It has block scope</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Variables will be hoisted</td>
+        <td>Hoisted but not initialized</td>
+        <td></td>
+      </tr>
+    </table>
--- javascript interviews Question/typefunctions.js
+++ javascript interviews Question/typefunctions.js
@@ -28,5 +28,23 @@ salamu_Message(salamuByName, 'Good Morning', "Today you look lovely", "KIM")
 //============================ Unary functions ======================================
 const Unaryfunction = (a) => console.log(a + 10)
 // =========================== Currying Functions ===================================
-const CurryingFunction = (a, b, c) => console.log(a)
+const multiarg = (a, b, c) => a + b + c 
+console.log(multiarg(1, 2, 3));
+const CurryingFunction = (a) =>  (b) =>  (c) => a => b => c
+console.log(CurryingFunction(1)); // returns a function 1 + b +c
+console.log(CurryingFunction(1) (2)); // returns a function c = 3 +c
+console.log(CurryingFunction(1)(2)(3)); // returns a number 
+
 // =========================== Pure Functions =======================================
+// IMPURE
+const numArr = []
+const impurefunction = (number) => numArr.push(number)
+
+//PURE FUNCTIONS
+const pureFunction = (number) => (ArrNumItems) => ArrNumItems.concat([number])
+
+//Display the results
+console.log('impure fun',impurefunction(6)); // returns 1
+console.log(numArr); // returns [6]
+console.log(pureFunction(7)(numArr)); // returns [6, 7]
+console.log(numArr); // returns [6]
