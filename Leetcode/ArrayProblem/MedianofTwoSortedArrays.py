4e524e0ad2e692603c20d4cdfffed3fdc889ab1b
elijah <elijahkungu100@gmail.com>
2023-02-10 18:42:27 +0300

    mean two sorted

--- Leetcode/ArrayProblem/MedianofTwoSortedArrays.py
+++ Leetcode/ArrayProblem/MedianofTwoSortedArrays.py
@@ -42,6 +42,27 @@ def findMedianSortedArrays(nums1, nums2):
         
         print(median)
 
+def findMedianSortedArrays1(nums1, nums2):
+    all_arr = []
+    for i in nums1:
+        all_arr.append(i)
+    for j in nums2:
+        all_arr.append(j)
+    
+    print(sorted(all_arr))
+    sorted_all_arr = sorted(all_arr)
+    arr_median = int(len(sorted_all_arr)/2)
+
+    if arr_median % 2 != 0:
+        print(sorted_all_arr[arr_median])
+    else:
+        arr_median = (sorted_all_arr[arr_median] + sorted_all_arr[arr_median] +1 ) / 2
+        print(arr_median)
+
+
+
+    
+
 if __name__ == "__main__":
-    findMedianSortedArrays(  nums1 = [0,0], nums2 = [0,0])
+    findMedianSortedArrays1([1,3], nums2 = [2, 10])
 
