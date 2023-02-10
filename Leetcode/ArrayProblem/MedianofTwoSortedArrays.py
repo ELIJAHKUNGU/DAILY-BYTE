71c078387323b35d5b47f59b845aea33a3760f4e
elijah <elijahkungu100@gmail.com>
2023-02-10 09:22:56 +0300

    median two sorted arrays

---
+++ Leetcode/ArrayProblem/MedianofTwoSortedArrays.py
@@ -0,0 +1,47 @@
+"""'
+MEDIAN OF TWO SORTED ARRAYS
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+Example 1:
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+Example 3:
+Input: nums1 = [0,0], nums2 = [0,0]
+Output: 0.00000
+Example 4:
+Input: nums1 = [], nums2 = [1]
+Output: 1.00000
+
+'"""
+import itertools
+def findMedianSortedArrays(nums1, nums2):
+    all_arr = []
+    combined_arr = itertools.chain(nums1,nums2)
+    # sort_Arr = combined_arr.sort()
+    # print(sort_Arr)
+    for i in combined_arr:
+        all_arr.append(i)
+    
+#    sort
+    sorted_arr =sorted(all_arr)
+    median = int(len(sorted_arr) / 2)
+    print(median)
+   
+
+    if (median % 2 != 0):
+        print(sorted_arr[median])
+    else:
+        # print(sorted_arr[median-1],  sorted_arr[median])
+
+        median = (sorted_arr[median-1] +sorted_arr[median]) / 2
+        
+        print(median)
+
+if __name__ == "__main__":
+    findMedianSortedArrays(  nums1 = [0,0], nums2 = [0,0])
+
